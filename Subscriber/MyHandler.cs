91c01a71778129a748adbe16322a7569ddbf6d19
Ripan Paul <ripanpaul2000@gmail.com>
2017-11-21 16:06:42 +0530

    test commit

---
+++ Subscriber/MyHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Messages;
+
+public class MyHandler :
+    IHandleMessages<RowMessage>
+{
+    public Task Handle(RowMessage message, IMessageHandlerContext context)
+    {
+        
+
+
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,45 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messages;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitiazeQueue("testqueue.Subscriber", "testqueue.Publisher").GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        // InitiazeQueue
+        static async Task InitiazeQueue(string EndpointName_Subscriber, string EndpointName_Publisher)
+        {
+            var endpointConfiguration = new EndpointConfiguration(EndpointName_Subscriber);
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+            var routing = transport.Routing();
+            routing.RegisterPublisher(
+                assembly: typeof(RowMessage).Assembly,
+                publisherEndpoint: EndpointName_Publisher);
+
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
